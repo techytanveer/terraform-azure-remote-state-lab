f2257a6023d8e602a8394be33657ba93eee69a4e
Techy Tanveer <tanvahme@gmail.com>
2026-03-01 16:02:11 +1300

    fix: syntax issues

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -40,6 +40,7 @@ module "network" {
   resource_group_name   = module.resource_group.name
   vnet_address_space    = var.vnet_address_space
   subnet_address_prefix = var.subnet_address_prefix
+  admin_ip_cidr         = var.admin_ip_cidr
   tags                  = local.common_tags
 }
 
